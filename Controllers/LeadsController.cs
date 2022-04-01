b445a5b0603d1b0cb26e7b74323124a019f651c2
MartinCote5 <martin.cote2@hotmail.com>
2022-04-01 17:53:09 -0400

    Merge branch 'development' into martin

--- Controllers/LeadsController.cs
+++ Controllers/LeadsController.cs
@@ -51,6 +51,7 @@ namespace RocketElevatorREST.Controllers
             }
             
             var leadListResult = await _context.leads.Where(x => !(customerEmailList.Contains(x.e_mail)) && leadCreationDateList.Contains(x.created_at)).ToListAsync();
+            leadListResult = leadListResult.OrderBy(x => x.Id).ToList();
             return leadListResult;
         }
     }
