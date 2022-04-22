bd396d24da50ab664a12e8277f1670750f3113a4
MartinCote5 <martin.cote2@hotmail.com>
2022-04-22 12:30:03 -0400

    view is done

--- Models/Building.cs
+++ Models/Building.cs
@@ -5,7 +5,7 @@ namespace RocketElevatorREST.Models
         // TODO
         public long Id { get; set; }
         public long customer_id { get; set; }
-         public string? full_name_of_the_building_administrator {get; set;}
+        public string? full_name_of_the_building_administrator {get; set;}
         public string? email_of_the_administrator_of_the_building {get; set;}
         public string? phone_number_of_the_building_administrator {get; set;}
 
