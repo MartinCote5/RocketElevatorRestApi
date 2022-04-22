f0e0d0b8242784d09ec3630f441c30e309e8132a
MartinCote5 <martin.cote2@hotmail.com>
2022-04-22 10:24:47 -0400

    product model done and query for the customers working

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,9 +3,9 @@ namespace RocketElevatorREST.Models
     public class Product {
         public Customer? customer {get; set;}
         public List<Building>? buildings {get; set;}
-        public List<Column>? columns {get; set;}
         public List<Battery>? batteries {get; set;}
-        public List<Elevator>? elevators {get; set;}
+        public List<Column>? columns {get; set;}
+         public List<Elevator>? elevators {get; set;}
     }
     
 }
